e1df2221a7f746ea679cc6c32cc0bf8abd89f52b
alanpereiradasilva <161897310+alanpereiradasilva@users.noreply.github.com>
2025-03-22 09:24:17 -0300

    Criei o arquivo

--- ex115/sistema.py
+++ ex115/sistema.py
@@ -1,10 +1,17 @@
 from lib.interface import *
+from arquivo import *
 from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
 
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
     if resposta == 1:
         cabeçalho("Opção 1")
+        lerArquivo(arq)
     elif resposta == 2:
         cabeçalho("Opção 2")
     elif resposta == 3:
